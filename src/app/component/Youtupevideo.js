3d1e1826b6872d11b09909d1c6659d250d81afbc
EANOK R <165556014+eanok07@users.noreply.github.com>
2025-07-04 16:26:52 +0530

    width & height updated (#1)

--- src/app/component/Youtupevideo.js
+++ src/app/component/Youtupevideo.js
@@ -7,13 +7,37 @@ export default function Youtupevideo() {
   return (
     <>
       <Container>
-        <Box height="calc(100vh - 92px)" alignContent="center">
-          <Box textAlign="center">
+        <Box height="calc(100vh - 90px)" alignContent="center" pt={5}>
+          <Box textAlign="center" display={{ xs: "none", md: "block" }}>
             <YouTube
               loading="eager"
               videoId="tjXsBWNJlYU"
               id="ytplayer"
               opts={{
+                width: "100%",
+                height: "600px",
+                playerVars: {
+                  autoplay: true,
+                  modestbranding: 1,
+                  controls: 0,
+                  enablejsapi: 1,
+                  rel: 0,
+                },
+              }}
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            />
+          </Box>{" "}
+          <Box textAlign="center" display={{ xs: "block", md: "none" }}>
+            <YouTube
+              loading="eager"
+              videoId="tjXsBWNJlYU"
+              id="ytplayer"
+              opts={{
+                width: "100%",
+                height: "400px",
                 playerVars: {
                   autoplay: true,
                   modestbranding: 1,
