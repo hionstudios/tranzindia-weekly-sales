87cb3bc9e4c186813240ddc33fb0553b3b74f0e2
EANOK R <165556014+eanok07@users.noreply.github.com>
2025-07-08 18:33:21 +0530

    og image updated (#2)
    * og image updated
    
    * og content uptaded

--- src/util/meta.js
+++ src/util/meta.js
@@ -10,35 +10,48 @@ const meta = pathname => {
     og = "/og.png"
   } = metadata[pathname];
 
-  const url = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  const url = "https://tranzindia-bbs.vercel.app";
 
   return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      url: `${url}${pathname}`,
-      siteName: "Hion Studios",
-      images: [`${url}`],
-      type: "website",
-    },
-    metadataBase: new URL(url),
-    alternates: {
-      canonical: `${url}${pathname}`,
-    },
-    twitter: {
-      card: "summary_large_image",
-      title,
-      description,
-      creator: "@hion_studios",
-    },
-    robots: {
-      index,
-      follow,
-      nocache,
-    },
-  };
+        category: 'technology',
+        title,
+        description,
+        authors: [
+            {
+                name: "Karthikeyan K"
+            },
+            {
+                name: "Rajaganapathy D"
+            },
+            {
+                name: "Hion Studios"
+            },
+        ],
+        creator: "Karthikeyan, Hion Studios",
+        openGraph: {
+            title,
+            description,
+            url: `${url}${pathname}`,
+            siteName: 'Hion Studios',
+            type: 'website',
+            images: [`${url}${og}`]
+        },
+        metadataBase: new URL(url),
+        alternates: {
+            canonical: `${url}${pathname}`,
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title,
+            description,
+            creator: '@hion_studios',
+        },
+        robots: {
+            index,
+            follow,
+            nocache,
+        },
+    }
 };
 
 export default meta;
